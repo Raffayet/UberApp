064d0438544a5d6703b51de8a2c932ddd8ad9b46
jovancevic <jovancevic.sw1.2019@uns.ac.rs>
2023-01-29 13:21:41 +0100

    Angular login service test

--- uber-frontend/src/app/modules/auth/services/login.service.spec.ts
+++ uber-frontend/src/app/modules/auth/services/login.service.spec.ts
@@ -1,16 +1,46 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { LoginService } from './login.service';
+import { filledLoginForm, tokenResponse } from '../mocks/login';
+import { of } from 'rxjs';
 
 describe('LoginService', () => {
   let service: LoginService;
+  let httpTestingController: HttpTestingController;
+
+  let url = 'http://localhost:8081/api';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(LoginService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should login and return the appropriate token', () => {
+    let success: boolean = false;
+
+    service.logIn(filledLoginForm, success).subscribe((res) => {
+      expect(res).toEqual(tokenResponse);
+    });
+
+    const req = httpTestingController.expectOne({
+      method: 'POST',
+      url: `${url}/auth/login`,
+    });
+
+    req.flush(tokenResponse);
+
+  });
+
+
 });
